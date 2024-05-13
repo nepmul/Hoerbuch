4fc1899a47e676e34a1daf30f3cca7a5c54f2efc
nepmul <nepmul@proton.me>
2024-05-13 20:57:41 +0200

    soweit fertig, entwicklung eingestellt, da monatliches limit von 10000 char

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
 from epub2txt import epub2txt
+from os import mkdir
 
-max_characters = 500
+
+ebook_name="Queen of Shadows.epub"
+
+max_characters = 1000
 
 characters={
 
     "Ã¼": "ü",
     "Ã¤": "ä",
     "Ã¶": "ö",
-    "Ã":"ß",    #muss an letzter stelle stehen, da nur ein zeichen
+    "Ã": "ß",
+    "â": "'"#muss an letzter stelle stehen, da nur ein zeichen
 }
 
 
@@ -21,12 +26,15 @@ headers = {
   "xi-api-key": "971662cf6e00425c0e7dc507397ad586"
 }
 
+book_dir=ebook_name.split(".")[0]
+try:
+    mkdir(book_dir)
+except:
+    pass
 
 
 
-
-
-ebook = epub2txt("ebook.epub", outputlist=False)
+ebook = epub2txt(ebook_name, outputlist=False)
 
 for key in characters:
     ebook=ebook.replace(key, characters[key])
@@ -37,10 +45,17 @@ sentences=ebook.split(".")
 
 sections=[""]
 for i in sentences:
-    if len(sections[-1]) + len(i) >= max_characters:
+    if len(sections[-1]) + len(i) >= max_characters -1:
         sections.append("")
     sections[-1]+=i+". "
 
+
+for e, i in enumerate(sections):
+    if len(i) > max_characters:
+        print(f"Section too long: {len(i)} > {max_characters} \n {i}")
+
+if input(f"{e} sections, commit? (Y)") != "Y":
+    quit()
 
 
 for e, i in enumerate(sections):
@@ -54,12 +69,12 @@ for e, i in enumerate(sections):
       }
     }
 
-    print(i)
-    print(len(i))
+    print(f'{book_dir}/{book_dir}_{e}')
 
 
     response = requests.post(url, json=data, headers=headers)
-    with open(f'Testbuch_{e}', 'wb') as f:
+    with open(f'{book_dir}/{book_dir}_{e}', 'wb') as f:
         for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
             if chunk:
                 f.write(chunk)
+
